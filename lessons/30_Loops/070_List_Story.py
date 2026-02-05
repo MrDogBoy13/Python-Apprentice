d895f2beaf80819faf9da801e285807ae7c58a66
MrDogBoy13 <hildreth.ezra@gmail.com>
2026-02-05 02:32:51 +0000

    ALMOST finished list story

--- lessons/30_Loops/070_List_Story.py
+++ lessons/30_Loops/070_List_Story.py
@@ -8,9 +8,13 @@ append them to the story
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
     'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
 
-story = []
+story = (words[0], words[2], words[7], words[9], words[1], words[14], words[13], words[5], words[6], words[3], words[14], words[12], words[14], words[19], words[15], words[10], words[8], words[4], words[17], words[8], words[18], )
 
 # Create a story using the words in the list
 
 # Display the story to the user
-print(' '.join(story))
+print(str(story))
+
+
+
+
